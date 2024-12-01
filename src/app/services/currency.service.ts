887d56e9972efc90053d19808709fc287d79bc4b
Zyther <gregocm1000@gmail.com>
2024-12-01 19:52:45 -0300

    Some style improvements.

--- src/app/services/currency.service.ts
+++ src/app/services/currency.service.ts
@@ -1,12 +1,16 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { ICurrency } from '../interfaces/currency';
 import { environment } from '../../environments/environment.development';
 import { IConversion } from '../interfaces/conversion';
+import { UserService } from './user.service';
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CurrencyService {
+  authService = inject(AuthService);
+  userService = inject(UserService);
   currencies: ICurrency[] = [];
 
   constructor() {
@@ -30,11 +34,27 @@ export class CurrencyService {
   }
 
   async convert(conversionData: IConversion) {
-    var usd = 1;
-    var toUSD = (conversionData.amount * conversionData.fromCurrency) / usd;
-    var result = (toUSD / conversionData.toCurrency) * usd;
-    result = Math.round((result + Number.EPSILON) * 100) / 100;
+    try {
+      var fromRate = this.currencies.find(
+        (c) => c.id == conversionData.fromCurrency
+      )?.exchangeRate;
+      var toRate = this.currencies.find(
+        (c) => c.id == conversionData.toCurrency
+      )?.exchangeRate;
 
-    return result;
+      if (!fromRate || !toRate) {
+        throw new Error('Rates not found.');
+      }
+
+      const usd = 1;
+      var toUSD = (conversionData.amount * fromRate) / usd;
+      var result = (toUSD / toRate) * usd;
+      result = Math.round((result + Number.EPSILON) * 100) / 100;
+
+      return result;
+    } catch (error) {
+      console.error('Error during conversion: ', error);
+      throw error;
+    }
   }
 }
